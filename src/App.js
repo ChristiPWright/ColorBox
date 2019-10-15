350a5992d5bc603a5dcb78e5385f2222077241f1
Ubuntu <ubuntu@ip-172-31-40-102.us-east-2.compute.internal>
2019-10-15 23:21:50 +0000

    actual file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+///currently not looping correctly - index is not changing
+  class App extends React.Component {
+    constructor(props){
+      super(props)
+        this.state = {
+          color: 'white',
+          colorArray: ['white', 'green', 'blue', 'yellow', 'red', 'purple', 'orange']
+        }
+    }
+
+ handleChange = () => {
+    let newColor 
+    let colorArray = ['white', 'green', 'blue', 'yellow', 'red', 'purple', 'orange']
+    if (colorArray.indexOf(this.state.color) === 6){
+      newColor = colorArray[0]
+    } else {
+      newColor = colorArray[colorArray.indexOf(this.state.color)+1]
+    }
+     return this.setState({color: newColor})
+  }
+  
+  // return in the for loop returns arr[0] and after arr[-1]
+  arrayChange = () => {
+    let colorArray = ['white', 'green', 'blue', 'yellow', 'red', 'purple', 'orange']
+    colorArray.forEach(element => element)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2>Welcome to the World of Color!</h2>
+          <h4>Click the below square to discover colors.</h4>
+          <button 
+            style = {{backgroundColor: this.state.color}} 
+            onClick = {this.arrayChange}
+          >
+            {this.state.color}
+          </button>
+        </header>
+      </div>
+    );
+  }//render end 
+} //ends calss component
 
 export default App;
